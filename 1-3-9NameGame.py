b337deaf76acae89b301503b0e904d0a67bb1005
Mr Castro <30452915+castro1019@users.noreply.github.com>
2019-02-05 08:01:41 -0500

    Add files via upload

---
+++ 1-3-9NameGame.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+'''Alter the code to produce a different output if your name is entered.  As always, start a log file
+test your code.  When it works, upload the code without changing the file name.  Upload the log file to
+the classroom alonf with your code'''
+
+def who_is_this():
+    name=raw_input('Please enter the name of someone in this class:')
+    
+    if name=='Carlos':
+        print(name,' favorite phrase is : "Now the fun part is..."')
+    
+    return
+    
